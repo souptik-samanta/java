3162fe16383bdee395c91d21b9b8601b93679721
Souptik Samanta <161435060+souptik-samanta@users.noreply.github.com>
2024-08-07 15:07:48 +0530

    Update javaflowerpatt.java

--- javaflowerpatt.java
+++ javaflowerpatt.java
@@ -1,16 +1,22 @@
-public class FlowerPattern {
-    public static void main(String[] args) {
+public class FlowerPattern
+    {
+    public static void main(String[] args) 
+        {
         int n = 10; // Adjust this value to change the size of the flower
         int stemHeight = 12; // Height of the stem
         int potWidth = 8; // Width of the pot
         int potHeight = 4; // Height of the pot
 
         // Flower (X Shape)
-        for (int i = 0; i < 2 * n; i++) {
-            for (int j = 0; j < 2 * n; j++) {
-                if (i == j || i + j == 2 * n - 1) {
+        for (int i = 0; i < 2 * n; i++) 
+        {
+            for (int j = 0; j < 2 * n; j++)
+                {
+                if (i == j || i + j == 2 * n - 1)
+                {
                     System.out.print("*");
-                } else {
+                } else 
+                {
                     System.out.print(" ");
                 }
             }
@@ -18,9 +24,12 @@ public class FlowerPattern {
         }
 
         // Stem
-        for (int i = 0; i < stemHeight; i++) {
-            for (int j = 0; j < 2 * n; j++) {
-                if (j == n - 1 || j == n) {
+        for (int i = 0; i < stemHeight; i++)
+            {
+            for (int j = 0; j < 2 * n; j++) 
+            {
+                if (j == n - 1 || j == n) 
+                {
                     System.out.print("!");
                 } else {
                     System.out.print(" ");
@@ -31,9 +40,12 @@ public class FlowerPattern {
 
         // Pot
         int potStart = n - potWidth / 2;
-        for (int i = 0; i < potHeight; i++) {
-            for (int j = 0; j < 2 * n; j++) {
-                if (j >= potStart && j < potStart + potWidth) {
+        for (int i = 0; i < potHeight; i++)
+            {
+            for (int j = 0; j < 2 * n; j++)
+                {
+                if (j >= potStart && j < potStart + potWidth)
+                {
                     System.out.print("#");
                 } else {
                     System.out.print(" ");
